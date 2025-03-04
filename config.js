6352bddd2f0d55929bd42c0bd414a49523ef72eb
koyeb01125 <apnapremiummarket011@gmail.com>
2025-03-04 22:53:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     numberOfRegisterInOneTime: 6, // no of registration want to make in one command.
-    domains: ["lamoshitt.com", "crazzybabe.com"], // names of domain for the emails. 
+    domains: ["http://10minutemail.com", "https://temp-mail.org"], // names of domain for the emails. 
     oneTimeMailGen: 10 * 100000,
     telegramBotToken: "8194712392:AAEHwq1ENvJK6oeSno1pI6eth4jt58300eU", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
     adminNamesForTelegramBot: ["Adityamajumdaar","Cckillernew_bot"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
